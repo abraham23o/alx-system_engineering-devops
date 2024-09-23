cc364e04e89f13b48dc499ea573a6000702f9992
abraham23o <abraonyonka23@gmail.com>
2024-09-23 11:57:42 +0300

    get number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+returns the number of subscribers (not active users, 
+total subscribers) for a given subreddit. If an invalid 
+subreddit is given, the function should return 0
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """Fetch reddit subs"""
+    url = f'https://api.reddit.com/r/{subreddit}/about'
+    headers = {'User-Agent': 'api_advanced/0-subs (by u/adari'}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code==200:
+        data = response.json() or {}
+        print(data)
+        return data.get('data', {}).get('subscribers', 0)
+    return 0
